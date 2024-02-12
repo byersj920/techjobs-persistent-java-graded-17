5e51c82369d95b797f7a4e5d8b00fae9c8dd9f75
Jonathan <byersj920@gmail.com>
2024-02-12 14:16:29 -0600

    Task 1 complete. Task 2 in progress

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,31 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @Size(min=0, max = 255)
+    private String location;
+
+    public Employer (){
+        super();
+    }
+
+    public Employer(String location){
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,16 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public Skill () {super();}
+
+    public Skill (String description){
+        super();
+        this.description = description;
+    }
 }
